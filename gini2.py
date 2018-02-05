df1604859050e32e48ad73966adaffa51c3d300f
taojiawei <xbynet@outlook.com>
2018-02-05 11:15:30 +0800

    ui

--- gini2.py
+++ gini2.py
@@ -3,8 +3,10 @@ import xlwings as xw
 # from functools import reduce
 
 pi=0.1
+group=10
+excel_path=r'E:/城镇化协调性数据.xlsx'
 #连接到excel
-workbook = xw.Book(r'E:/基尼系数测度数据.xlsx')
+workbook = xw.Book(excel_path)
 sheet_names=[workbook.sheets[i].name for i in range(0,len(workbook.sheets),1)]
 rng=workbook.sheets[0].range('A1').expand()
 nrows=rng.rows.count-1
@@ -29,11 +31,11 @@ for si in range(0,len(sheet_names),1):
         data_range=workbook.sheets[sheet_names[si]].range(c+'2:'+c+str(nrows+1))
         data=data_range.value
         data=sorted(data)
-        step=len(data)//10
-        remainder=len(data)%10
+        step=len(data)//group
+        remainder=len(data)%group
         split_d=[data[i*step+(remainder if i>=remainder else i):i*step+(remainder if i>=remainder else i)+step+(0 if i>=remainder else 1)] for i in range(0,10,1)]
         d_ce=[sum(split_d[i]) for i in range(0,len(split_d),1)]
-        print(sheet_names[si]+':'+str('%.f' % workbook.sheets[sheet_names[si]].range(c+'1').value)+':'+str(gini_my2(d_ce)))
+        print(sheet_names[si]+':'+str(workbook.sheets[sheet_names[si]].range(c+'1').value)+':'+str(gini_my2(d_ce)))
 
 # #连接到指定单元格
 # data_range = workbook.sheets('超效率').range('J1:J285')
